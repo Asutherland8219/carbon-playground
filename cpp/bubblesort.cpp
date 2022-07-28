112d4c2ecef504b738cfc7ae94148629c5523ee0
Alex <asutherland8219@gmail.com>
2022-07-28 20:34:35 -0300

    shite this is i tell ya the compiler not debugging

--- cpp/bubblesort.cpp
+++ cpp/bubblesort.cpp
@@ -1,4 +1,4 @@
-#include  <iosteam>
+#include <iostream>
 using namespace std;
 
 int main()
@@ -7,12 +7,27 @@ int main()
 	int j;
 	int temp;
 
-	int arr[8] = {12, 3,1,5,18,10,7,35}
+	int arr[8] = {12, 3,1,5,18,10,7,35};
 	cout<<"Unsorted array :\n";
 
-	for(int i=0; i<8)
+	for(int i=0;i<8;i++)
+
+	{
+		cout<<arr[i]<<"\t";
+	{
+	cout<<endl;
+
+	for(int i=0;i<8;i++)
+	{
+		for(int j=i+1;j<8;j++)
+		{
+			if (arr[j]<arr[i])
+				{
+					temp=arr[i];
+					arr[i]=arr[j];
+					arr[j]=temp;
+				}
+			}
+		}
+	}
 }
-
-
-
-
