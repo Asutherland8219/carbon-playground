c238d7aeefc4cce541e49881cf93653b93558574
Alex <asutherland8219@gmail.com>
2022-08-06 08:07:18 -0300

    change some endl to \n to follow best practices

--- cpp/learning/bio.cpp
+++ cpp/learning/bio.cpp
@@ -9,11 +9,12 @@ int main()
     int age{29};
 
     std::cout<<"Hello, my name is Alex. I am " << age;
-    std::cout<<" years old." << std::endl;
+    std::cout<<" years old." << std::endl; // the endl function is very clunky. Instead use \n
 
     std::cout<<"I am currently " << weight;
-    std::cout<< "kg standing at " << height << "cm tall" << std::endl;
+    std::cout<< "kg standing at " << height << "cm tall.\n";
 
     std::cout<<"The purpose of this is to practice printing statements and logic";
-    std::cout<<" of the CPP compiler. " << std::endl;
+    std::cout<<" of the CPP compiler.\n"; // the results remain the same 
+
 }
