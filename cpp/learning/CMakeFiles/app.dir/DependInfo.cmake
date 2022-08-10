af8ddb8e3d6ba798074384562cbcd75163178ffe
Alex <asutherland8219@gmail.com>
2022-08-10 08:06:00 -0300

    started messing around with makefiles

---
+++ cpp/learning/CMakeFiles/app.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/suds171/Documents/carbon-playground/cpp/learning/multi_file_projects/add.cpp" "CMakeFiles/app.dir/add.o" "gcc" "CMakeFiles/app.dir/add.o.d"
+  "/home/suds171/Documents/carbon-playground/cpp/learning/multi_file_projects/main.cpp" "CMakeFiles/app.dir/main.o" "gcc" "CMakeFiles/app.dir/main.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- cpp/learning/multi_file_projects/main.cpp
+++ cpp/learning/multi_file_projects/main.cpp
@@ -1,8 +1,16 @@
 # include <iostream>
+# include "add.h"
+
+int add(int x, int y); // add forward declaration so we know it exists
 
 int main()
 {
     std::cout<<"Guten Tag, World"; //testing comments 
+    int x {5};
+    int y {2};
+
+    std::cout << "The sum of 3 and 4 is: " << add(x, y) << '\n';
+
     return 0;
 }
 
